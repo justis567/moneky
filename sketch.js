1c780af8ddd2a8b9ea20db2274a057845aef5bbb
justis567 <74329165+justis567@users.noreply.github.com>
2021-03-03 12:01:47 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+var backImage,backgr;
+var player, player_running;
+var ground,ground_img;
+var stone,stone_Img
+
+var END =0;
+var PLAY =1;
+var gameState = PLAY;
+
+function preload(){
+  backImage=loadImage("jungle.jpg");
+  player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
+stone_Img=loadImage("stone.png")
+
+}
+
+function setup() {
+  createCanvas(800,400);
+  
+  backgr=createSprite(0,0,800,400);
+  backgr.addImage(backImage);
+  backgr.scale=1.5;
+  backgr.x=backgr.width/2;
+  backgr.velocityX=-4;
+  
+  player = createSprite(100,340,20,50);
+  player.addAnimation("Running",player_running);
+  player.scale = 0.1;
+  
+  ground = createSprite(400,350,800,10);
+  ground.x=ground.width/2;
+  ground.visible=false;
+  
+}
+
+function draw() { 
+  background(0);
+
+  if(gameState===PLAY){
+  
+  if(backgr.x<100){
+    backgr.x=backgr.width/2;
+  }
+  
+
+
+    if(keyDown("space") ) {
+      player.velocityY = -12;
+    }
+    player.velocityY = player.velocityY + 0.8;
+  
+    player.collide(ground);
+
+  }
+
+  drawSprites();
+}
+
+function spawnstone() {
+  if(frameCount%80===0) {
+     stone=createSprite(900,330,20,20)
+  stone.addImage(stone_Img)
+  stone.scale = 0.1
+  stone.velocityX = -7  
+    stone.lifetime = 120
+    
+  
+  }
+}
